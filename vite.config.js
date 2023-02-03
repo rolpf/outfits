cec527e8288913bd60c13c9aa794fbae2e55516e
rolpf <jiroxane@hotmail.fr>
2023-02-03 19:15:59 +0100

    init

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import { readFileSync } from 'node:fs'
+const host = 'outfits.traefik.me';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: [
+                ...refreshPaths,
+                'app/Http/Livewire/**',
+            ],
+        }),
+    ],
+    server: {
+        host: host,
+        // Create a certificate and put it in your ~/.certs folder
+        // https://deliciousbrains.com/ssl-certificate-authority-for-local-https-development/
+        https: {
+            key: readFileSync('/home/roxane/certs/_wildcard.traefik.me-key.pem'),
+            cert: readFileSync('/home/roxane/certs/_wildcard.traefik.me.pem'),
+        }
+
+    },
+});
