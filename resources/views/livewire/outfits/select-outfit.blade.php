408a705da3f8827065d105ba981eaf20078bf80f
rolpf <jiroxane@hotmail.fr>
2023-05-06 14:52:23 +0200

    mobile navbar init

--- resources/views/livewire/outfits/select-outfit.blade.php
+++ resources/views/livewire/outfits/select-outfit.blade.php
@@ -1,15 +1,14 @@
 
 <div class="w-full">
     
-
-        <input wire:model="search" type="text" placeholder="Enter your outfit name" class="w-full border rounded shadow-sm px-8 py-2 my-2">
+        <input wire:model="search" type="text" placeholder="{{ __('Search for and outfit') }}" class="w-full border rounded shadow-sm px-8 py-2 my-2">
         <div class=" grid grid-cols-2">
         @foreach ($outfits as $outfit)
             <div wire:click="$set('selectedOutfit', {{ $outfit->id }})" @class([
-                "h-8 w-8 bg-red-200",
+                "h-32 w-32 bg-red-200",
                 "bg-green-200" => $selectedOutfit == $outfit->id
             ])>
-                {{ $outfit->id }}
+                <img src="{{ $outfit->thumbnail }}">
                 
             </div>
         @endforeach
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="py-12">
+    <div class="py-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
@@ -7,18 +7,18 @@
                 <form method="POST" action="{{ route('post.store') }}">
                     @csrf
                     <h1 class="font-bold text-xl my-4">{{ __("Send a post") }}</h1>
+                    <button class="block border rounded shadow-sm bg-white px-8 py-2 font-semibold"> {{ __('Add Outfit') }}</button>
                     <textarea type="text" name="content" placeholder="What's new?" class="w-full resize-none"></textarea>
-
-                    <button class="block border rounded shadow-sm bg-white px-8 py-2 font-semibold"> Add Outfit </button>
                     
-                    <div class="w-full">
+                    <button type="submit" class="border rounded shadow-sm bg-white w-full px-16 py-4 font-semibold my-2 ">{{ __('Send') }}</button>
+                    <div class="mb-16">
                         <livewire:outfits.select-outfit />
                     </div>
 
                     @error('content')
                         <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
-                    <button type="submit" class="absolute block border rounded shadow-sm bg-white w-full px-16 py-4 font-semibold bottom-0">Send</button>
+        
                 </form>
             </div>
         </div>
